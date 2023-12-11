7d1c4089f9ba035c2e3fc617a4c3bd7b57eeb9d4
Achyuth Sambaraju <achyuthsambaraju@gmail.com>
2023-12-11 02:44:27 -0800

    Final Changes

--- achyuth_sambaraju/settings.py
+++ achyuth_sambaraju/settings.py
@@ -88,8 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -100,12 +98,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
